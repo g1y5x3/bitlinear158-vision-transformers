890bae5691e8e87daf537dff8038154ef09bce07
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-05-18 17:34:39 +0000

    prepare for tests refactor

---
+++ tests/test_vit.py
@@ -0,0 +1,5 @@
+from PIL import Image
+import requests
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
--- train_vit.py
+++ train_vit.py
@@ -1,3 +1,2 @@
 from datasets import load_dataset
-
 dataset = load_dataset("imagenet-1k", cache_dir="/workspace/imagenet")
