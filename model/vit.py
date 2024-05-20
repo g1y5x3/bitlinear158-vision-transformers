05810f8d5ea78e6d19f1ce3a67cbbe5d86bdc129
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-05-20 04:41:15 +0000

    continue work on vit

--- model/vit.py
+++ model/vit.py
@@ -1,5 +1,11 @@
+import torch
 from torch import nn
 from transformer import ViTEncoderLayer
 
 class ViT(nn.modules):
-  def __init__(self, )
+  def __init__(self, image_size=224, channels=3, patch_size=16, d_model=768):
+    super().__init__()
+    # assume images to be square
+    num_patches = (image_size // patch_size)**2
+    patch_dim = channels * (patch_size**2)
+    self.position_embedding = nn.Parameter(torch.randn(1, num_patches+1, d_model))
